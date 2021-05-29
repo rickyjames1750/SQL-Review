ae5ee030aef5cc2fe16d4bda22026fe803e48677
rickyjames1750 <rickyjames1750@gmail.com>
2021-05-29 01:37:11 -0700

    This commit will combine the (Group By) and (Join) statements as well as integrate (alias) statements among the tables

--- useDatabase.sql
+++ useDatabase.sql
@@ -87,3 +87,8 @@ SELECT SUM(release_year) FROM albums;
 
 SELECT band_id, COUNT(band_id) FROM albums 
 GROUP BY band_id;
+
+SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+FROM bands AS b 
+LEFT JOIN albums AS a ON b.id = a.band_id
+GROUP BY b.id; 
